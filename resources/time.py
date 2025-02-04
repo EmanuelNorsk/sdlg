c3dcbecb8a53012dbd47ba888d3ee695566e815c
Emanuel Hesselroth <emanuelodenhesselroth@gmail.com>
2025-02-04 14:47:11 +0100

    ye

--- resources/time.py
+++ resources/time.py
@@ -5,11 +5,26 @@ class Clock:
     def __init__(self):
         self.time = sdl3.SDL_GetPerformanceCounter()
         self.last_time = sdl3.SDL_GetPerformanceCounter()
-
+        self.delta = 0
+        self.maxFPS = -1
+        self.elapsed = 0
+        self.draw = 1
         self.freq = sdl3.SDL_GetPerformanceFrequency()
 
 
-    def tick(self):
+    def tick(self, cap = -1):
+        if cap > 0:
+            self.maxFPS = cap
+        self.delta = ((self.time - self.last_time) / self.freq)
+        if self.maxFPS > 0:
+            if self.elapsed <= (1 / self.maxFPS):
+                self.elapsed += self.delta
+                self.draw = 1
+            else:
+                self.elapsed -= (1 / self.maxFPS)
+                self.draw = 0
+
+
         self.last_time = self.time
 
 
